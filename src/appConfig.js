3ac1b76d28cd65ca5864f58dcf2614c35cac3351
turner <douglass.turner@gmail.com>
2026-04-02 08:48:38 -0400

    custom assembly registry: now testing with annotation tracks in bigbed format hosted at turner aws s3. Looks good,

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,6 +1,8 @@
 const appConfig = {
-    // customAssemblyRegistryURL: '/custom-assemblies-12-cloudflare-proxy-github-annotation-haplotype.json',
-    customAssemblyRegistryURL: '/custom-assemblies-12-s3-cors-enabled.json',
+    // customAssemblyRegistryURL: '/custom-assemblies-12-s3-cors-enabled.json',
+    customAssemblyRegistryURL: '/custom-assemblies-12-s3-cors-enabled-bigbed-s3.json',
+    // customAssemblyRegistryURL: '/custom-assemblies-12-s3-cors-enabled-bigbed.json',
+
     preload: {
         enabled: false,
         locus: 'chr1:25240000-25460000'
